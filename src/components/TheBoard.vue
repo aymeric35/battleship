16c8dba75f425a5f486a09fd6a6651ec5f36fadd
aymeric35 <Oshi2606@gmail.com>
2023-06-28 18:18:58 +0200

    ✨ Add TheBoard.vue

---
+++ src/components/TheBoard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type Gameboard from '~/factories/Gameboard'
+
+defineProps<{
+  gameboard: ReturnType<typeof Gameboard>
+}>()
+</script>
+
+<template>
+  <div class="container">
+    <div v-for="(x, i) in gameboard.board" :key="i" :data-x="x">
+      <div v-for="(y, j) in x" :key="j" class="b-1 b-blue b-solid" :data-y="y" @click="$emit('attack', i, j)" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container > div {
+  display: grid;
+  grid-template-columns: repeat(10, minmax(25px, 75px));
+  justify-content: center;
+}
+
+.container div[data-y] {
+  aspect-ratio: 1/1;
+}
+
+.container div[data-y="-2"] {
+  background-color: red
+}
+.container div[data-y="-3"] {
+  background-color: blue
+}
+</style>
